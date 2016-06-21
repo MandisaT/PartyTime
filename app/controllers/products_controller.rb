b06dd0d397b38f4a4677bf465f2bb862c9f71e7f
MandisaT <mandisa.thomas1@gmail.com>
2016-06-21 16:50:10 -0400

    made changes to show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,29 +1,44 @@
 class ProductsController < ApplicationController
 	def index
-    	@products = Product.all
+    	@event = Event.where(id: params[:event_id]).first
+# products that belong to this specific event find the event where the id is equal to the event_id
+    	@products = @event.products
   	end
-  	def new 
+ 	def new 
   		@product = Product.new
+  		@event = params[:event_id]  
  	end 
+
+  	def edit
+	  	@product =Product.find(params[:id]) 
+	end 
+ 
   
  	def update 
  		@product = Product.find(params[:id])
-		 @product.update(product_parmas) 
+		 @product.update(product_params) 
 		 redirect_to  product_path @product
  	end 
  	def destroy
 	   Product.find(params[:id]).destroy
-	    flash[:success] = "User deleted"
+	    flash[:success] = "product deleted"
 	    redirect_to new_product_path
 	
  	end 
+ 	def show 
+ 		@product =Product.find(params[:id])
+ 		@event = params[:id]  
+ 	end 
+ 
+
   	def create
 		@product = Product.new(product_params)
+		flash[:notice] = "Check you party cost"
 		# creating a new record in the system 
 		if @product.save
 			# saving the records 
 		
-			redirect_to products_path
+			redirect_to  event_path(@product.event)
 			# redirecting to the show page for this user 
 		else
 			redirect_to new_product_path
---
+++ db/migrate/20160621204442_add_guest_list_events.rb
@@ -0,0 +1,4 @@
+class AddGuestListEvents < ActiveRecord::Migration
+  def change
+  end
+end
